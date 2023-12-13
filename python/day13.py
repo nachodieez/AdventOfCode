151300a243b1efb7c080afa85af384350d41319e
nachodieez <nachodiez10@live.com>
2023-12-13 17:46:33 +0100

    Code simplified for day 13 2023

--- python/day13.py
+++ python/day13.py
@@ -1,43 +1,37 @@
 import numpy as np
 data = open("../data/day13.in").read().strip().split("\n\n")
 
-def get_reflections(pattern, part = 1):
-    sol = 0
+def get_reflections(pattern, part1 = True):
+    res = 0
     pattern = np.array([list(x) for x in pattern.split("\n")])
     rows, cols = pattern.shape
     for row in range(rows):
-        try:
-            first = pattern[:row,:][::-1,:]
-            second = pattern[row:,:]
-            m = min(first.shape[0], second.shape[0])
-            first = first[:m, :]
-            second = second[:m, :]
-            if part == 1:
-                if np.all([first[i,:] == second[i,:] for i in range(m)]):
-                    sol += row * 100
-            else:
-                if np.count_nonzero([first[i,:] != second[i,:] for i in range(m)]) == 1:
-                    sol += row * 100
-        except IndexError:
-            pass
+        first = pattern[:row,:][::-1,:]
+        second = pattern[row:,:]
+        m = min(first.shape[0], second.shape[0])
+        first = first[:m, :]
+        second = second[:m, :]
+        if part1:
+            if np.all([first[i,:] == second[i,:] for i in range(m)]):
+                res += row * 100
+        else:
+            if np.count_nonzero([first[i,:] != second[i,:] for i in range(m)]) == 1:
+                res += row * 100
     
     for col in range(cols):
-        try:
-            first = pattern[:,:col][:,::-1]
-            second = pattern[:,col:]
-            m = min(first.shape[1], second.shape[1])
-            first = first[:,:m]
-            second = second[:,:m]
-            if part == 1:
-                if np.all([first[:,i] == second[:,i] for i in range(m)]):
-                    sol += col
-            else:
-                if np.count_nonzero([first[:,i] != second[:,i] for i in range(m)]) == 1:
-                    sol += col
-        except IndexError:
-            pass
+        first = pattern[:,:col][:,::-1]
+        second = pattern[:,col:]
+        m = min(first.shape[1], second.shape[1])
+        first = first[:,:m]
+        second = second[:,:m]
+        if part1:
+            if np.all([first[:,i] == second[:,i] for i in range(m)]):
+                res += col
+        else:
+            if np.count_nonzero([first[:,i] != second[:,i] for i in range(m)]) == 1:
+                res += col
 
-    return sol
+    return res
 
-print("Sol 1:", sum([get_reflections(pattern) for pattern in data]))
-print("Sol 2:", sum([get_reflections(pattern, part=2) for pattern in data]))
+print("Sol 1:", sum(get_reflections(pattern) for pattern in data))
+print("Sol 2:", sum(get_reflections(pattern, part1=False) for pattern in data))
